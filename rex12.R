8f0485859f7b3de9259f9f1659f602a962d1d0a2
Prathyakshanaa <112697187+Prathyakshanaa@users.noreply.github.com>
2024-02-24 10:08:21 +0530

    Delete rex12.R

--- rex12.R
+++
@@ -1,8 +0,0 @@
-install.packages("readxl")
-diabetest1<-read_excel("C:\Users\prath\OneDrive\Documents\dibetics.xlsx")
-A<-c(diabetest1$Age)
-Mean<-mean(A)
-Std<-sd(A)
-Zscore<-(A-Mean)/Std
-Zscore
-
